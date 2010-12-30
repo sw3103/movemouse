7430f710aef15b358d9fdfa87a6b1b14fc37e6da
sw3103_cp <SND\sw3103_cp>
2010-12-30 14:20:12 +0000

    2.1.0 - Minor bug fixes and some extra keystroke features.

--- Move Mouse/MouseForm.Designer.cs
+++ Move Mouse/MouseForm.Designer.cs
@@ -39,6 +39,9 @@
             this.resumeCheckBox = new System.Windows.Forms.CheckBox();
             this.resumeNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
+            this.stealthCheckBox = new System.Windows.Forms.CheckBox();
+            this.keystrokeCheckBox = new System.Windows.Forms.CheckBox();
+            this.keystrokeComboBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.mousePictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.delayNumericUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resumeNumericUpDown)).BeginInit();
@@ -56,16 +59,16 @@
             // 
             // actionButton
             // 
-            this.actionButton.Location = new System.Drawing.Point(224, 180);
+            this.actionButton.Location = new System.Drawing.Point(41, 161);
             this.actionButton.Name = "actionButton";
             this.actionButton.Size = new System.Drawing.Size(75, 23);
-            this.actionButton.TabIndex = 1;
+            this.actionButton.TabIndex = 0;
             this.actionButton.Text = "Start";
             this.actionButton.UseVisualStyleBackColor = true;
             // 
             // countdownProgressBar
             // 
-            this.countdownProgressBar.Location = new System.Drawing.Point(12, 151);
+            this.countdownProgressBar.Location = new System.Drawing.Point(12, 203);
             this.countdownProgressBar.Name = "countdownProgressBar";
             this.countdownProgressBar.Size = new System.Drawing.Size(499, 17);
             this.countdownProgressBar.TabIndex = 2;
@@ -85,7 +88,7 @@
             0});
             this.delayNumericUpDown.Name = "delayNumericUpDown";
             this.delayNumericUpDown.Size = new System.Drawing.Size(53, 23);
-            this.delayNumericUpDown.TabIndex = 3;
+            this.delayNumericUpDown.TabIndex = 1;
             this.delayNumericUpDown.Value = new decimal(new int[] {
             30,
             0,
@@ -109,49 +112,44 @@
             this.moveMouseCheckBox.Location = new System.Drawing.Point(155, 41);
             this.moveMouseCheckBox.Name = "moveMouseCheckBox";
             this.moveMouseCheckBox.Size = new System.Drawing.Size(143, 20);
-            this.moveMouseCheckBox.TabIndex = 5;
+            this.moveMouseCheckBox.TabIndex = 2;
             this.moveMouseCheckBox.Text = "Move mouse pointer";
             this.moveMouseCheckBox.UseVisualStyleBackColor = true;
             // 
             // clickMouseCheckBox
             // 
             this.clickMouseCheckBox.AutoSize = true;
-            this.clickMouseCheckBox.Checked = true;
-            this.clickMouseCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.clickMouseCheckBox.Location = new System.Drawing.Point(155, 67);
+            this.clickMouseCheckBox.Location = new System.Drawing.Point(155, 93);
             this.clickMouseCheckBox.Name = "clickMouseCheckBox";
             this.clickMouseCheckBox.Size = new System.Drawing.Size(157, 20);
-            this.clickMouseCheckBox.TabIndex = 6;
+            this.clickMouseCheckBox.TabIndex = 4;
             this.clickMouseCheckBox.Text = "Click left mouse button";
             this.clickMouseCheckBox.UseVisualStyleBackColor = true;
             // 
             // autoPauseCheckBox
             // 
             this.autoPauseCheckBox.AutoSize = true;
-            this.autoPauseCheckBox.Checked = true;
-            this.autoPauseCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.autoPauseCheckBox.Location = new System.Drawing.Point(155, 93);
+            this.autoPauseCheckBox.Location = new System.Drawing.Point(155, 145);
             this.autoPauseCheckBox.Name = "autoPauseCheckBox";
             this.autoPauseCheckBox.Size = new System.Drawing.Size(304, 20);
-            this.autoPauseCheckBox.TabIndex = 7;
+            this.autoPauseCheckBox.TabIndex = 6;
             this.autoPauseCheckBox.Text = "Automatically pause when mouse pointer moved";
             this.autoPauseCheckBox.UseVisualStyleBackColor = true;
             // 
             // resumeCheckBox
             // 
             this.resumeCheckBox.AutoSize = true;
-            this.resumeCheckBox.Checked = true;
-            this.resumeCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.resumeCheckBox.Location = new System.Drawing.Point(155, 119);
+            this.resumeCheckBox.Location = new System.Drawing.Point(155, 171);
             this.resumeCheckBox.Name = "resumeCheckBox";
             this.resumeCheckBox.Size = new System.Drawing.Size(181, 20);
-            this.resumeCheckBox.TabIndex = 8;
+            this.resumeCheckBox.TabIndex = 7;
             this.resumeCheckBox.Text = "Automatically resume after";
             this.resumeCheckBox.UseVisualStyleBackColor = true;
             // 
             // resumeNumericUpDown
             // 
-            this.resumeNumericUpDown.Location = new System.Drawing.Point(334, 118);
+            this.resumeNumericUpDown.Enabled = false;
+            this.resumeNumericUpDown.Location = new System.Drawing.Point(334, 170);
             this.resumeNumericUpDown.Maximum = new decimal(new int[] {
             9999,
             0,
@@ -164,7 +162,7 @@
             0});
             this.resumeNumericUpDown.Name = "resumeNumericUpDown";
             this.resumeNumericUpDown.Size = new System.Drawing.Size(53, 23);
-            this.resumeNumericUpDown.TabIndex = 9;
+            this.resumeNumericUpDown.TabIndex = 8;
             this.resumeNumericUpDown.Value = new decimal(new int[] {
             300,
             0,
@@ -174,17 +172,91 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(390, 120);
+            this.label2.Location = new System.Drawing.Point(390, 172);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(122, 16);
             this.label2.TabIndex = 10;
             this.label2.Text = "seconds of inactivity";
             // 
+            // stealthCheckBox
+            // 
+            this.stealthCheckBox.AutoSize = true;
+            this.stealthCheckBox.Location = new System.Drawing.Point(155, 67);
+            this.stealthCheckBox.Name = "stealthCheckBox";
+            this.stealthCheckBox.Size = new System.Drawing.Size(322, 20);
+            this.stealthCheckBox.TabIndex = 3;
+            this.stealthCheckBox.Text = "Enable stealth mode (pointer movement not visible)";
+            this.stealthCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // keystrokeCheckBox
+            // 
+            this.keystrokeCheckBox.AutoSize = true;
+            this.keystrokeCheckBox.Location = new System.Drawing.Point(155, 119);
+            this.keystrokeCheckBox.Name = "keystrokeCheckBox";
+            this.keystrokeCheckBox.Size = new System.Drawing.Size(114, 20);
+            this.keystrokeCheckBox.TabIndex = 5;
+            this.keystrokeCheckBox.Text = "Send keystroke";
+            this.keystrokeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // keystrokeComboBox
+            // 
+            this.keystrokeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.keystrokeComboBox.Enabled = false;
+            this.keystrokeComboBox.FormattingEnabled = true;
+            this.keystrokeComboBox.Items.AddRange(new object[] {
+            "{BACKSPACE}",
+            "{BREAK}",
+            "{CAPSLOCK}",
+            "{DELETE}",
+            "{DOWN}",
+            "{END}",
+            "{ENTER}",
+            "{ESC}",
+            "{HELP}",
+            "{HOME}",
+            "{INSERT}",
+            "{LEFT}",
+            "{NUMLOCK}",
+            "{PGDN}",
+            "{PGUP}",
+            "{PRTSC}",
+            "{RIGHT}",
+            "{SCROLLLOCK}",
+            "{TAB}",
+            "{UP}",
+            "{F1}",
+            "{F2}",
+            "{F3}",
+            "{F4}",
+            "{F5}",
+            "{F6}",
+            "{F7}",
+            "{F8}",
+            "{F9}",
+            "{F10}",
+            "{F11}",
+            "{F12}",
+            "{F13}",
+            "{F14}",
+            "{F15}",
+            "{F16}",
+            "{ADD}",
+            "{SUBTRACT}",
+            "{MULTIPLY}",
+            "{DIVIDE}"});
+            this.keystrokeComboBox.Location = new System.Drawing.Point(275, 117);
+            this.keystrokeComboBox.Name = "keystrokeComboBox";
+            this.keystrokeComboBox.Size = new System.Drawing.Size(138, 24);
+            this.keystrokeComboBox.TabIndex = 11;
+            // 
             // MouseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 214);
+            this.ClientSize = new System.Drawing.Size(523, 235);
+            this.Controls.Add(this.keystrokeComboBox);
+            this.Controls.Add(this.keystrokeCheckBox);
+            this.Controls.Add(this.stealthCheckBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.resumeNumericUpDown);
             this.Controls.Add(this.resumeCheckBox);
@@ -224,5 +296,8 @@
         private System.Windows.Forms.CheckBox resumeCheckBox;
         private System.Windows.Forms.NumericUpDown resumeNumericUpDown;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox stealthCheckBox;
+        private System.Windows.Forms.CheckBox keystrokeCheckBox;
+        private System.Windows.Forms.ComboBox keystrokeComboBox;
     }
 }
--- Move Mouse/MouseForm.cs
+++ Move Mouse/MouseForm.cs
@@ -23,9 +23,11 @@ namespace Ellanet
         delegate void UpdateCountdownProgressBarDelegate(ref ProgressBar pb, int delay, int elapsed);
         delegate void ButtonPerformClickDelegate(ref Button b);
         delegate Point GetControlScreenLocationDelegate(ref Control c);
+        delegate object ComboBoxGetSelectedItemDelegate(ref ComboBox cb);
+        delegate int ComboBoxGetSelectedIndexDelegate(ref ComboBox cb);
 
         [Flags]
-        enum MouseEventFlags
+        enum MouseEventFlags : int
         {
             LEFTDOWN = 0x00000002,
             LEFTUP = 0x00000004,
@@ -37,6 +39,14 @@ namespace Ellanet
             RIGHTUP = 0x00000010
         }
 
+        [Flags]
+        enum Win32Consts
+        {
+            INPUT_MOUSE = 0,
+            INPUT_KEYBOARD = 1,
+            INPUT_HARDWARE = 2,
+        }
+
         [StructLayout(LayoutKind.Sequential)]
         struct LASTINPUTINFO
         {
@@ -45,6 +55,22 @@ namespace Ellanet
             public int cbSize;
             [MarshalAs(UnmanagedType.U4)]
             public int dwTime;
+        }
+
+        struct INPUT
+        {
+            public int type;
+            public MOUSEINPUT mi;
+        }
+
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public int dwExtraInfo;
         }
 
         [DllImport("user32.dll")]
@@ -60,11 +86,17 @@ namespace Ellanet
             ref LASTINPUTINFO plii);
 
         [DllImport("user32.dll")]
-        private static extern bool SystemParametersInfo(
+        static extern bool SystemParametersInfo(
             int uAction,
             int uParam,
             ref int lpvParam,
             int flags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(
+            uint nInputs,
+            ref INPUT pInputs,
+            int cbSize);
 
         public MouseForm()
         {
@@ -79,12 +111,23 @@ namespace Ellanet
             resumeTimer.Interval = 1000;
             resumeTimer.Tick += new EventHandler(resumeTimer_Tick);
             resumeCheckBox.CheckedChanged += new EventHandler(resumeCheckBox_CheckedChanged);
+            keystrokeCheckBox.CheckedChanged += new EventHandler(keystrokeCheckBox_CheckedChanged);
             int screenSaverTimeout = GetScreenSaverTimeout();
 
             if (screenSaverTimeout > 0)
             {
                 resumeNumericUpDown.Value = (screenSaverTimeout / 2);
             }
+        }
+
+        void keystrokeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked) && (!keystrokeCheckBox.Checked))
+            {
+                keystrokeCheckBox.Checked = true;
+            }
+
+            keystrokeComboBox.Enabled = keystrokeCheckBox.Checked;
         }
 
         void resumeCheckBox_CheckedChanged(object sender, EventArgs e)
@@ -101,6 +144,8 @@ namespace Ellanet
 
         void resumeTimer_Tick(object sender, EventArgs e)
         {
+            //Debug.WriteLine(GetLastInputTime().ToString());
+
             if (GetLastInputTime() > resumeNumericUpDown.Value)
             {
                 ButtonPerformClick(ref actionButton);
@@ -114,12 +159,7 @@ namespace Ellanet
 
         void clickMouseCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            //if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked) && (actionButton.Text == "Pause"))
-            //{
-            //    ButtonPerformClick(ref actionButton);
-            //}
-
-            if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked))
+            if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked) && (!keystrokeCheckBox.Checked))
             {
                 clickMouseCheckBox.Checked = true;
             }
@@ -127,15 +167,12 @@ namespace Ellanet
 
         void moveMouseCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            //if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked) && (actionButton.Text == "Pause"))
-            //{
-            //    ButtonPerformClick(ref actionButton);
-            //}
-
-            if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked))
+            if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked) && (!keystrokeCheckBox.Checked))
             {
                 moveMouseCheckBox.Checked = true;
             }
+
+            stealthCheckBox.Enabled = moveMouseCheckBox.Checked;
         }
 
         void MouseForm_FormClosing(object sender, FormClosingEventArgs e)
@@ -167,21 +204,21 @@ namespace Ellanet
                     break;
                 default:
 
-                    if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked))
-                    {
-                        MessageBox.Show("Please select at least on mouse action (move or click).", "No Mouse Actions Enabled", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    }
-                    else
-                    {
-                        resumeTimer.Stop();
-                        moveMouseThread = new Thread(new ThreadStart(MoveMouseThread));
-                        moveMouseThread.Start();
-                        actionButton.Text = "Pause";
-                        this.Opacity = .75;
-                        this.WindowState = FormWindowState.Normal;
-                        this.TopMost = true;
-                        startTime = DateTime.Now;
-                    }
+                    //if ((!moveMouseCheckBox.Checked) && (!clickMouseCheckBox.Checked))
+                    //{
+                    //    MessageBox.Show("Please select at least on mouse action (move or click).", "No Mouse Actions Enabled", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    //}
+                    //else
+                    //{
+                    resumeTimer.Stop();
+                    moveMouseThread = new Thread(new ThreadStart(MoveMouseThread));
+                    moveMouseThread.Start();
+                    actionButton.Text = "Pause";
+                    this.Opacity = .75;
+                    this.WindowState = FormWindowState.Normal;
+                    this.TopMost = true;
+                    startTime = DateTime.Now;
+                    //}
 
                     break;
             }
@@ -216,50 +253,45 @@ namespace Ellanet
 
                     if (moveMouseCheckBox.Checked)
                     {
-                        const int mouseMoveLoopSleep = 1;
-                        const int mouseSpeed = 10;
-
-                        Point originalMousePoint = Cursor.Position;
-                        Control pbToControl = (Control)mousePictureBox;
-                        Cursor.Position = GetControlScreenLocation(ref pbToControl);
-
-                        for (int i = 0; i < mousePictureBox.Width; i += mouseSpeed)
+                        if (!stealthCheckBox.Checked)
                         {
-                            Cursor.Position = new Point(Cursor.Position.X + mouseSpeed, Cursor.Position.Y);
-                            Thread.Sleep(mouseMoveLoopSleep);
+                            const int mouseMoveLoopSleep = 1;
+                            const int mouseSpeed = 1;
+                            const int moveSquareSize = 10;
+
+                            for (int i = 0; i < moveSquareSize; i += mouseSpeed)
+                            {
+                                MoveMouse(new Point(1, 0));
+                                Thread.Sleep(mouseMoveLoopSleep);
+                            }
+
+                            for (int i = 0; i < moveSquareSize; i += mouseSpeed)
+                            {
+                                MoveMouse(new Point(0, 1));
+                                Thread.Sleep(mouseMoveLoopSleep);
+                            }
+
+                            for (int i = 0; i < moveSquareSize; i += mouseSpeed)
+                            {
+                                MoveMouse(new Point(-1, 0));
+                                Thread.Sleep(mouseMoveLoopSleep);
+                            }
+
+                            for (int i = 0; i < moveSquareSize; i += mouseSpeed)
+                            {
+                                MoveMouse(new Point(0, -1));
+                                Thread.Sleep(mouseMoveLoopSleep);
+                            }
                         }
-
-                        Point topRight = GetControlScreenLocation(ref pbToControl);
-                        topRight.Offset(mousePictureBox.Width, 0);
-                        Cursor.Position = topRight;
-
-                        for (int i = 0; i < mousePictureBox.Height; i += mouseSpeed)
+                        else
                         {
-                            Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y + mouseSpeed);
-                            Thread.Sleep(mouseMoveLoopSleep);
+                            MoveMouse(new Point(0, 0));
                         }
+                    }
 
-                        Point bottomRight = GetControlScreenLocation(ref pbToControl);
-                        bottomRight.Offset(mousePictureBox.Width, mousePictureBox.Height);
-                        Cursor.Position = bottomRight;
-
-                        for (int i = 0; i < mousePictureBox.Height; i += mouseSpeed)
-                        {
-                            Cursor.Position = new Point(Cursor.Position.X - mouseSpeed, Cursor.Position.Y);
-                            Thread.Sleep(mouseMoveLoopSleep);
-                        }
-
-                        Point bottomLeft = GetControlScreenLocation(ref pbToControl);
-                        bottomLeft.Offset(0, mousePictureBox.Height);
-                        Cursor.Position = bottomLeft;
-
-                        for (int i = 0; i < mousePictureBox.Height; i += mouseSpeed)
-                        {
-                            Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y - mouseSpeed);
-                            Thread.Sleep(mouseMoveLoopSleep);
-                        }
-
-                        Cursor.Position = originalMousePoint;
+                    if ((keystrokeCheckBox.Checked) && (ComboBoxGetSelectedIndex(ref keystrokeComboBox) > -1))
+                    {
+                        SendKeys.SendWait(ComboBoxGetSelectedItem(ref keystrokeComboBox).ToString());
                     }
 
                     secondsElapsed = 0;
@@ -313,6 +345,30 @@ namespace Ellanet
             }
         }
 
+        object ComboBoxGetSelectedItem(ref ComboBox cb)
+        {
+            if (InvokeRequired)
+            {
+                return (object)Invoke(new ComboBoxGetSelectedItemDelegate(ComboBoxGetSelectedItem), new object[] { cb });
+            }
+            else
+            {
+                return cb.SelectedItem;
+            }
+        }
+
+        int ComboBoxGetSelectedIndex(ref ComboBox cb)
+        {
+            if (InvokeRequired)
+            {
+                return (int)Invoke(new ComboBoxGetSelectedIndexDelegate(ComboBoxGetSelectedIndex), new object[] { cb });
+            }
+            else
+            {
+                return cb.SelectedIndex;
+            }
+        }
+
         int GetLastInputTime()
         {
             int idleTime = 0;
@@ -336,6 +392,21 @@ namespace Ellanet
             int value = 0;
             SystemParametersInfo(SPI_GETSCREENSAVERTIMEOUT, 0, ref value, 0);
             return value;
+        }
+
+        void MoveMouse(Point point)
+        {
+            MOUSEINPUT mi = new MOUSEINPUT();
+            mi.dx = point.X;
+            mi.dy = point.Y;
+            mi.mouseData = 0;
+            mi.time = 0;
+            mi.dwFlags = Convert.ToInt32(MouseEventFlags.MOVE);
+            mi.dwExtraInfo = 0;
+            INPUT input = new INPUT();
+            input.mi = mi;
+            input.type = Convert.ToInt32(Win32Consts.INPUT_MOUSE);
+            SendInput(1, ref input, 28);
         }
     }
 }
