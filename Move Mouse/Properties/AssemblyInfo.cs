ff16146b430c93ad5dae512e628134ed38de7b95
Steve Williams <steve@windandkite.co.uk>
2018-10-09 23:29:30 +0100

    3.5.0 release.

--- Move Mouse/Properties/AssemblyInfo.cs
+++ Move Mouse/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.0.*")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.5.0.*")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
