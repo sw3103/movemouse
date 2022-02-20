86fa999425aab562126f720ccd061032ceed48c4
Steve Williams <stevewilliamsuk@outlook.com>
2022-02-20 09:49:38 +0000

    Reorganising in preparation for 4x upload.

--- Move Mouse/Classes/CelebrityMouse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ellanet.Classes
-{
-    public class CelebrityMouse
-    {
-        public string Name { get; set; }
-        public string ImageName { get; set; }
-        public string ToolTip { get; set; }
-    }
-}
